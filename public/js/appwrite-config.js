bcf1346f99e18d6f78a0d441ea7b350d6f127567
Hendri040921 <rotusanto@gmail.com>
2025-06-06 21:18:26 +0700

    appwrite-config.js

--- public/js/appwrite-config.js
+++ public/js/appwrite-config.js
@@ -1,11 +1,11 @@
-<script type="module">
-  import { Client, Account, Databases } from 'https://cdn.jsdelivr.net/npm/appwrite@13.0.1/+esm';
+;import { Client, Account, Databases } from "appwrite";
 
-  const client = new Client();
-  client
-    .setEndpoint('https://fra.cloud.appwrite.io/v1') // Ganti sesuai server kamu
-    .setProject('684042be002f1040b027');
+const client = new Client();
+client
+  .setEndpoint("https://fra.cloud.appwrite.io/v1") // GANTI sesuai project kamu
+  .setProject("684042be002f1040b027"); // GANTI dengan Project ID kamu
 
-  const account = new Account(client);
-  // Lanjutkan penggunaan...
-</script>
+const account = new Account(client);
+const databases = new Databases(client);
+
+export { client, account, databases };
